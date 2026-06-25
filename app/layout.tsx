bbdd1dcf0557c97dbefa8633f01cd6de3c94c813
Parker Kuykendall <parkerkuyken@gmail.com>
2026-06-24 18:10:23 -0600

    Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,10 @@ const queryClient = new QueryClient();
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+    
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🔨</text></svg>" />
+  </head>
       <body>
         <WagmiProvider config={wagmiConfig}>
           <QueryClientProvider client={queryClient}>
